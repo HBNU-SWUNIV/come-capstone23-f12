153fa9496545de95ca9c7066334f356b795c3bb9
tadaHP <gusvy0220@gmail.com>
2023-04-12 00:30:17 +0900

    feat: UserApiController 작성 및 UserApiControllerTests 작성 - application.yml 에 data.sql 초기 데이터 삽입 관련 설정 추가     - main application.yml: embedded 모드에서 데이터 추가 및 spring 컨테이너가 작성된 이후에 데이터를 넣기 위해 defer-datasource-initialization 값을 true 로 변경     - test application.yml: 테스트시에는 data.sql 데이터를 넣지 않기 위해 init.mode 를 never 로 설정 - UserApiController 에 유저 조회, 수정, 삭제 로직 추가, session 에 담긴 user 의 id와 요청한 user 의 id가 같을경우에만 허용     - 조회: 조회시 UserSearchDto 에 맞춰 값 리턴     - 수정: 수정시 body 에 담겨있는 x-www-form-urlencoded 에 맞춰 값 변경, 존재하지 않을시 변경하지 않는다.            또한 변경 후 데이터 조회로 리다이렉트     - 삭제: 데이터 삭제시 text 로 done 리턴     - 기타         1. postMan 사용시 session 을 얻어오기 위한 test 용 api 작성

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/api/user/UserApiController.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/api/user/UserApiController.java
@@ -2,17 +2,28 @@ package io.f12.notionlinkedblog.api.user;
 
 import static io.f12.notionlinkedblog.api.EmailApiController.*;
 
+import java.net.URI;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.f12.notionlinkedblog.domain.user.dto.info.UserSearchDto;
 import io.f12.notionlinkedblog.domain.user.dto.signup.UserSignupRequestDto;
 import io.f12.notionlinkedblog.domain.user.dto.signup.UserSignupResponseDto;
 import io.f12.notionlinkedblog.service.user.UserService;
@@ -42,5 +53,55 @@ public class UserApiController {
 		if (isVerified == null) {
 			throw new IllegalStateException("이메일 검증이 되지 않았습니다.");
 		}
+	}
+
+	@GetMapping(value = "/{id}")
+	public UserSearchDto getUserInfo(@PathVariable Long id) {
+		return userService.getUserInfo(id);
+	}
+
+	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<?> editUserInfo(
+		HttpSession session,
+		@PathVariable Long id,
+		@RequestParam(value = "username", required = false) String username,
+		@RequestParam(value = "email", required = false) String email,
+		@RequestParam(value = "password", required = false) String password,
+		@RequestParam(value = "profile", required = false) String profile,
+		@RequestParam(value = "blogTitle", required = false) String blogTitle,
+		@RequestParam(value = "githubLink", required = false) String githubLink,
+		@RequestParam(value = "instagramLink", required = false) String instagramLink,
+		@RequestParam(value = "introduction", required = false) String introduction
+	) {
+		Optional<UserSearchDto> sessionUser = Optional.ofNullable((UserSearchDto)session.getAttribute(session.getId()));
+		if (sessionUser.isPresent() && sessionUser.get().getId().equals(id)) {
+			userService.editUserInfo(id, username, email, password, profile, blogTitle,
+				githubLink, instagramLink, introduction);
+		}
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(URI.create("/api/users/" + id));
+		return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteUserInfo(@PathVariable Long id, HttpSession session) {
+		Optional<UserSearchDto> sessionUser = Optional.ofNullable((UserSearchDto)session.getAttribute(session.getId()));
+		if (sessionUser.isPresent() && sessionUser.get().getId().equals(id)) {
+			userService.removeUser(id);
+		}
+		return "done";
+	}
+
+	/**
+	 * 테스트용  api
+	 * @param id id에 해당하는 세션 가져오기
+	 */
+
+	@GetMapping("/getSession/{id}")
+	private String getSessionForTest(HttpSession session, @PathVariable Long id) {
+		UserSearchDto userInfo = userService.getUserInfo(id);
+		String sessionId = session.getId();
+		session.setAttribute(sessionId, userInfo);
+		return sessionId;
 	}
 }
--- 003 Code/BE/notion-linked-blog/src/test/java/io/f12/notionlinkedblog/api/user/UserApiControllerTests.java
+++ 003 Code/BE/notion-linked-blog/src/test/java/io/f12/notionlinkedblog/api/user/UserApiControllerTests.java
@@ -2,7 +2,10 @@ package io.f12.notionlinkedblog.api.user;
 
 import static io.f12.notionlinkedblog.api.EmailApiController.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.servlet.http.Cookie;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,11 +15,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.f12.notionlinkedblog.domain.user.User;
+import io.f12.notionlinkedblog.domain.user.dto.info.UserSearchDto;
 import io.f12.notionlinkedblog.domain.user.dto.signup.UserSignupRequestDto;
 
 @AutoConfigureMockMvc
@@ -59,5 +65,81 @@ class UserApiControllerTests {
 				resultActions.andExpect(status().isCreated());
 			}
 		}
+	}
+
+	@DisplayName("유저 정보 가져오기")
+	@Test
+	void getUserInfoTest() throws Exception {
+		ResultActions resultActions = mockMvc.perform(get("/api/users/1"))
+			.andDo(print());
+		resultActions
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+	}
+
+	@DisplayName("유저 정보 수정")
+	@Test
+	void editUserInfoTest() throws Exception {
+		final String fakeSessionId = "fakeSessionId";
+		final Long fakeUserId = 1L;
+		final String url = "/api/users/1";
+		User beforeUser = User.builder()
+			.username("user1")
+			.email("before@test.com")
+			.password("1234")
+			.build();
+		UserSearchDto changedDto = UserSearchDto.builder()
+			.username("changed")
+			.email("changed@test.com")
+			.build();
+
+		ReflectionTestUtils.setField(beforeUser, "id", fakeUserId);
+		ReflectionTestUtils.setField(changedDto, "id", fakeUserId);
+
+		String requestBody = om.writeValueAsString(changedDto);
+		MockHttpSession mockHttpSession = new MockHttpSession();
+		mockHttpSession.setAttribute(fakeSessionId, beforeUser);
+		ResultActions resultActions = mockMvc.perform(
+				put(url)
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.cookie(new Cookie("JSESSIONID", fakeSessionId))
+					.content(requestBody))
+			.andDo(print());
+
+		resultActions.andExpectAll(
+			status().is3xxRedirection(),
+			redirectedUrl(url)
+		);
+
+	}
+
+	@DisplayName("유저 정보 삭제")
+	@Test
+	void removeUserInfoTest() throws Exception {
+		final String fakeSessionId = "fakeSessionId";
+		final Long fakeUserId = 1L;
+		final String url = "/api/users/1";
+		User removedUser = User.builder()
+			.username("user1")
+			.email("before@test.com")
+			.password("1234")
+			.build();
+
+		ReflectionTestUtils.setField(removedUser, "id", fakeUserId);
+
+		MockHttpSession mockHttpSession = new MockHttpSession();
+		mockHttpSession.setAttribute(fakeSessionId, removedUser);
+		ResultActions resultActions = mockMvc.perform(
+				delete(url)
+					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+					.cookie(new Cookie("JSESSIONID", fakeSessionId))
+			)
+			.andDo(print());
+
+		resultActions.andExpectAll(
+			status().isOk(),
+			content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN)
+		);
 	}
 }
