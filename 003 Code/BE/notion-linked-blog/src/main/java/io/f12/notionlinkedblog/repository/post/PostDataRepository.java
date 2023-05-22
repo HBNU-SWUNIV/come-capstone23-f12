aeb1c67bb068acf094e7a085e91faf86a168f9ac
tadaHP <gusvy0220@gmail.com>
2023-05-23 01:42:44 +0900

    feat: PostDataRepository 에 Like 관련 테이블 fetchJoin 함   - 현재 페이징과 fetchJoin 을 같이 하고 있어 오류가 뜸, PR 전에 고칠 예정

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/repository/post/PostDataRepository.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/repository/post/PostDataRepository.java
@@ -14,23 +14,31 @@ import io.f12.notionlinkedblog.domain.post.Post;
 @Repository
 public interface PostDataRepository extends JpaRepository<Post, Long> {
 	@Query("SELECT p "
-		+ "FROM Post p join fetch p.user u "
+		+ "FROM Post p join fetch p.user left join fetch p.likes "
+		+ "WHERE p.title LIKE %:name%")
+		//TODO: HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
+	Slice<Post> findByTitle(@Param("name") String name, Pageable pageable);
+
+	@Query("SELECT p "
+		+ "FROM Post p join fetch p.user u left join fetch p.likes "
+		+ "WHERE p.content LIKE %:content%")
+		//TODO: HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
+	Slice<Post> findByContent(@Param("content") String content, Pageable pageable);
+
+	@Query("SELECT DISTINCT p "
+		+ "FROM Post p join fetch p.user left join fetch p.likes "
 		+ "WHERE p.id = :id")
 	Optional<Post> findById(@Param("id") Long id);
 
 	@Query("SELECT p "
-		+ "FROM Post p join fetch p.user u "
-		+ "WHERE p.title LIKE %:name%")
-	Slice<Post> findByTitle(@Param("name") String name, Pageable pageable);
-
-	@Query("SELECT p "
-		+ "FROM Post p join fetch p.user u "
+		+ "FROM Post p join fetch p.user u left join fetch p.likes "
 		+ "ORDER BY p.createdAt DESC")
+		//TODO: HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
 	Slice<Post> findLatestByCreatedAtDesc(Pageable pageable);
 
 	@Query("SELECT p "
-		+ "FROM Post p join fetch p.user u "
-		+ "WHERE p.content LIKE %:content%")
-	Slice<Post> findByContent(@Param("content") String content, Pageable pageable);
-
+		+ "FROM Post p join fetch p.user u left join fetch p.likes "
+		+ "ORDER BY p.popularity DESC")
+		//TODO: HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
+	Slice<Post> findPopularityByViewCountAtDesc(Pageable pageable);
 }
