a84fff23b43130d048f1c20c3af8619aa493efe0
Kyle <kyleruiz1515@gmail.com>
2023-05-28 21:35:22 +0900

    feat: 로그인 오류 처리

--- 003 Code/FE/notion-linked-blog/src/components/auth/LoginForm.jsx
+++ 003 Code/FE/notion-linked-blog/src/components/auth/LoginForm.jsx
@@ -18,6 +18,7 @@ export default function LoginForm({switchForm, setIsModalOpen}) {
 	const [email, onChangeEmail] = handleInput("");
 	const [password, onChangePassword] = handleInput("");
 	const [loading, setLoading] = useState(false);
+	const [isCorrect, setIsCorrect] = useState(true);
 	const dispatch = useAppDispatch();
 
 	const handleSubmit = async () => {
@@ -27,8 +28,10 @@ export default function LoginForm({switchForm, setIsModalOpen}) {
 
 			dispatch(login(response.data.user));
 			setIsModalOpen(false);
+			setIsCorrect(true);
 		} catch (e) {
 			console.log("로그인 실패", e);
+			setIsCorrect(false);
 		} finally {
 			setLoading(false);
 		}
@@ -42,17 +45,16 @@ export default function LoginForm({switchForm, setIsModalOpen}) {
 				<Form.Item
 					label="이메일"
 					name="email"
-					rules={[{required: true, pattern: /\S/g, message: "이메일은 필수 입력사항입니다"}]}
 				>
 					<Input onChange={onChangeEmail} placeholder="이메일을 입력하세요" value={email}/>
 				</Form.Item>
 				<Form.Item
 					label="비밀번호"
 					name="password"
-					rules={[{required: true, pattern: /\S/g, message: "비밀번호는 필수 입력사항입니다"}]}
 				>
 					<Input.Password onChange={onChangePassword} placeholder="비밀번호를 입력하세요" value={password}/>
 				</Form.Item>
+				{isCorrect === true ? null : <div style={{textAlign:"center", color:"red"}}>이메일 또는 비밀번호가 일치하지 않습니다</div>}
 				<StyledFormItem>
 					<Button type="primary" htmlType="submit" onClick={handleSubmit} loading={loading}>로그인</Button>
 				</StyledFormItem>
