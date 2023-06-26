a25262fb533ca3493297d97c6da38dd6549971ad
JIYONG JUNG <jeidiiy98@gmail.com>
2023-06-26 18:07:31 +0900

    feat: 댓글 작성 컴포넌트 개발

---
+++ 003 Code/FE/notion-linked-blog/src/components/post/comment/CommentContainer.tsx
@@ -0,0 +1,25 @@
+import {Typography} from "antd";
+import styled from "styled-components";
+
+import CommentForm from "./CommentForm";
+
+const StyledSection = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 768px;
+
+	@media screen and (max-width: 768px) {
+		width: 100vw;
+		padding: 0 1rem;
+	}
+`;
+
+export default function CommentContainer() {
+	return (
+		<StyledSection>
+			<Typography.Title level={4}>4개의 댓글</Typography.Title>
+			<CommentForm />
+		</StyledSection>
+	);
+}
