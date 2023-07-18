e1a6d50edace111618380e698af8dda421202b69
JIYONG JUNG <jeidiiy98@gmail.com>
2023-07-18 19:29:25 +0900

    refactor: 컴포넌트를 내부로 옮겨 복잡성 제거

--- 003 Code/FE/notion-linked-blog/src/components/post/comment/Comment.tsx
+++ 003 Code/FE/notion-linked-blog/src/components/post/comment/Comment.tsx
@@ -1,45 +1,120 @@
+import Image from "next/image";
 import {Typography} from "antd";
 import {PlusSquareOutlined} from "@ant-design/icons";
 import styled from "styled-components";
-import CommentWriterDetails from "./CommentWriterDetails";
+import {CommentGetResponse, requestDeleteCommentAPI} from "@/apis/comment";
+import {useSelector} from "react-redux";
+import {UserState} from "@/redux/userSlice";
+import {RootState} from "@/redux/store";
 
 const Container = styled.div`
-	border-top: 0.8px solid rgb(205, 205, 205);
-	padding: 1.5rem;
+  border-top: 0.8px solid rgb(205, 205, 205);
+  padding: 1.5rem;
 
-	@media screen and (max-width: 768px) {
-		padding: 1rem;
-	}
+  @media screen and (max-width: 768px) {
+    padding: 1rem;
+  }
 `;
 
 const StyledParagraph = styled(Typography.Paragraph)`
-	font-size: 1.125rem;
+  font-size: 1.125rem;
 
-	@media screen and (max-width: 768px) {
-		font-size: 1rem;
-	}
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 const StyledText = styled(Typography.Text)`
-	font-size: 1rem;
+  font-size: 1rem;
 
-	@media screen and (max-width: 768px) {
-		font-size: 0.875rem;
-	}
+  @media screen and (max-width: 768px) {
+    font-size: 0.875rem;
+  }
 
-	:hover {
-		cursor: pointer;
-	}
+  :hover {
+    cursor: pointer;
+  }
 `;
 
-export default function Comment() {
+const CommentWriterDetail = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1.5rem;
+`;
+
+const CommentWriterAvatar = styled(Image)`
+  @media screen and (max-width: 768px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
+
+const CommentWritingInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+`;
+
+const CommentWriter = styled(Typography.Text)`
+  font-size: 1rem;
+  font-weight: bold;
+
+  @media screen and (max-width: 768px) {
+    font-size: 0.875rem;
+  }
+`;
+
+const CommentModifier = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+
+  span {
+    color: #868E96;
+    font-size: 0.875rem;
+    cursor: pointer;
+    margin-left: 0.5rem;
+  }
+`;
+
+interface CommentProp {
+	values: CommentGetResponse;
+	postId: number;
+}
+
+export default function Comment({values, postId}: CommentProp) {
+	const {comment, createdAt, author, authorId, authorProfileLink, child} = values;
+	const {user} = useSelector<RootState, UserState>(state => state.user);
+
+	const handleClickDeletingBtn = async () => {
+		await requestDeleteCommentAPI(postId);
+	};
+
 	return (
 		<Container>
-			<CommentWriterDetails />
+			<CommentWriterDetail>
+				<CommentWriterAvatar
+					src={authorProfileLink}
+					width={54}
+					height={54}
+					alt="CommentWriterAvatar"
+				/>
+				<CommentWritingInfo>
+					<CommentWriter>{author}</CommentWriter>
+					<Typography.Text>{createdAt}</Typography.Text>
+				</CommentWritingInfo>
+				{user?.id === authorId &&
+					<CommentModifier>
+						<span>수정</span>
+						<span onClick={handleClickDeletingBtn}>삭제</span>
+					</CommentModifier>
+				}
+			</CommentWriterDetail>
 			<StyledParagraph>
-				댓글 내용입니다.
+				{comment}
 			</StyledParagraph>
-			<StyledText strong><PlusSquareOutlined /> 답글 달기</StyledText>
+			<StyledText strong><PlusSquareOutlined /> {child ? `${child.length}개의 답글` : "답글 달기"}</StyledText>
 		</Container>
 	);
 }
