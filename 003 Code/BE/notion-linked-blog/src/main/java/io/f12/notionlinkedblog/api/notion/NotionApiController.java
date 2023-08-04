2fd771b488d719c1498baef3edda1b9838f9fa5f
tadaHP <gusvy0220@gmail.com>
2023-08-04 17:05:04 +0900

    feat: 발표 준비를 위해 NotionApiController 에 사용하지 않는 매서드 주석 처리

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/api/notion/NotionApiController.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/api/notion/NotionApiController.java
@@ -1,32 +1,14 @@
 package io.f12.notionlinkedblog.api.notion;
 
 import static io.f12.notionlinkedblog.exceptions.message.ExceptionMessages.UserValidateMessages.*;
-import static org.springframework.http.MediaType.*;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.f12.notionlinkedblog.api.common.Endpoint;
-import io.f12.notionlinkedblog.domain.notion.dto.UpdateNotionPageInfoDto;
-import io.f12.notionlinkedblog.domain.post.dto.PostSearchDto;
-import io.f12.notionlinkedblog.exceptions.exception.NotionAuthenticationException;
 import io.f12.notionlinkedblog.security.login.ajax.dto.LoginUser;
 import io.f12.notionlinkedblog.service.notion.NotionService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -53,29 +35,29 @@ public class NotionApiController {
 	// public PostSearchDto getNotionPageToBlog(@Parameter(hidden = true) @AuthenticationPrincipal LoginUser loginUser,
 	// 	@RequestBody @Validated CreateNotionPageToBlogDto notionToBlogDto) throws NotionAuthenticationException {
 	// 	checkSameUser(notionToBlogDto.getUserId(), loginUser);
-	// 	return notionService.saveNotionPageToBlogDev(notionToBlogDto.getPath(), notionToBlogDto.getUserId());
+	// 	return notionService.saveNotionPageToBlog(notionToBlogDto.getPath(), notionToBlogDto.getUserId());
 	// }
 
-	@PostMapping("/setpage")
-	public void setPage(@Parameter(hidden = true) @AuthenticationPrincipal LoginUser loginUser,
-		@RequestPart(value = "pageId") String pageId) {
-		notionService.setNotionLinkPages(pageId, loginUser.getUser().getId());
-	}
+	// @PostMapping("/setpage")
+	// public void setPage(@Parameter(hidden = true) @AuthenticationPrincipal LoginUser loginUser,
+	// 	@RequestPart(value = "pageId") String pageId) {
+	// 	notionService.setNotionLinkPages(pageId, loginUser.getUser().getId());
+	// }
 
-	@PutMapping
-	@ResponseStatus(HttpStatus.OK)
-	@Operation(summary = "Notion 연동 포스트 수정", description = "id 에 해당하는 포스트 수정")
-	@ApiResponses(value = {
-		@ApiResponse(responseCode = "200", description = "포스트 수정 성공",
-			content = @Content(mediaType = APPLICATION_JSON_VALUE,
-				schema = @Schema(implementation = PostSearchDto.class,
-					description = "PostId 에 해당하는 사용자의 Post 를 노션과 업데이트, 업데이트시 모든 내용을 대체")))
-	})
-	public PostSearchDto updateNotionPageToBlog(@Parameter(hidden = true) @AuthenticationPrincipal LoginUser loginUser,
-		@RequestBody @Validated UpdateNotionPageInfoDto infoDto) throws NotionAuthenticationException {
-		checkSameUser(infoDto.getUserId(), loginUser);
-		return notionService.editNotionPageToBlogDev(infoDto.getUserId(), infoDto.getPostId());
-	}
+	// @PutMapping
+	// @ResponseStatus(HttpStatus.OK)
+	// @Operation(summary = "Notion 연동 포스트 수정", description = "id 에 해당하는 포스트 수정")
+	// @ApiResponses(value = {
+	// 	@ApiResponse(responseCode = "200", description = "포스트 수정 성공",
+	// 		content = @Content(mediaType = APPLICATION_JSON_VALUE,
+	// 			schema = @Schema(implementation = PostSearchDto.class,
+	// 				description = "PostId 에 해당하는 사용자의 Post 를 노션과 업데이트, 업데이트시 모든 내용을 대체")))
+	// })
+	// public PostSearchDto updateNotionPageToBlog(@Parameter(hidden = true) @AuthenticationPrincipal LoginUser loginUser,
+	// 	@RequestBody @Validated UpdateNotionPageInfoDto infoDto) throws NotionAuthenticationException {
+	// 	checkSameUser(infoDto.getUserId(), loginUser);
+	// 	return notionService.editNotionPageToBlogDev(infoDto.getUserId(), infoDto.getPostId());
+	// }
 
 	private void checkSameUser(Long id, LoginUser loginUser) {
 		if (!id.equals(loginUser.getUser().getId())) {
