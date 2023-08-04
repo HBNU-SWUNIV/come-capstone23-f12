79ad4013914d8ac031aa7e88bed676c4c3bc56fb
tadaHP <gusvy0220@gmail.com>
2023-08-04 17:04:30 +0900

    feat: NotionService, NotionOauthService 에 연동을 위한 매서드 추가 현재 NotionService에 Temp매서드들 수정 필요

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/service/notion/NotionService.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/service/notion/NotionService.java
@@ -4,7 +4,10 @@ import static io.f12.notionlinkedblog.exceptions.message.ExceptionMessages.Notio
 import static io.f12.notionlinkedblog.exceptions.message.ExceptionMessages.UserExceptionsMessages.*;
 import static io.f12.notionlinkedblog.exceptions.message.ExceptionMessages.UserValidateMessages.*;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.access.AccessDeniedException;
@@ -28,6 +31,7 @@ import lombok.extern.slf4j.Slf4j;
 import notion.api.v1.NotionClient;
 import notion.api.v1.model.blocks.Block;
 import notion.api.v1.model.blocks.Blocks;
+import notion.api.v1.model.search.SearchResult;
 import notion.api.v1.request.blocks.RetrieveBlockChildrenRequest;
 import notion.api.v1.request.blocks.RetrieveBlockRequest;
 
@@ -56,7 +60,7 @@ public class NotionService {
 	}
 
 	//아래 도메인 으로 묶기
-	public PostSearchDto saveNotionPageToBlogDev(String path, Long userId) throws NotionAuthenticationException {
+	public PostSearchDto saveNotionPageToBlog(String path, Long userId) throws NotionAuthenticationException {
 		String convertPath = convertPathToId(path);
 		Notion notion = notionDataRepository.findByPathValue(convertPath).orElse(null);
 		if (notion != null) {
@@ -95,28 +99,86 @@ public class NotionService {
 			.build();
 	}
 
-	public PostSearchDto editNotionPageToBlogDev(Long userId, Long postId) throws NotionAuthenticationException {
-		Post post = postDataRepository.findWithNotion(postId);
+	public void editNotionPageToBlog(Long userId, Post post) throws NotionAuthenticationException {
 		Long postUserId = post.getUser().getId();
 		checkSameUser(postUserId, userId);
 
-		String content = getContent(post.getNotion().getNotionId(), userId);
-		String title = getTitle(post.getNotion().getNotionId(), userId);
+		String content = getContent(post.getSyncedPages().getPageId(), userId);
+		String title = getTitle(post.getSyncedPages().getPageId(), userId);
 		post.editPost(title, content);
 
-		return PostSearchDto.builder()
-			.postId(post.getId())
-			.title(post.getTitle())
-			.content(post.getContent())
-			.viewCount(post.getViewCount())
-			.likes(post.getLikes().size())
-			.requestThumbnailLink(post.getStoredThumbnailPath())
-			.description(post.getDescription())
-			.createdAt(post.getCreatedAt())
-			.countOfComments(post.getComments().size())
-			.author(post.getUser().getUsername())
-			.isLiked(false)
-			.build();
+		// return PostSearchDto.builder()
+		// 	.postId(post.getId())
+		// 	.title(post.getTitle())
+		// 	.content(post.getContent())
+		// 	.viewCount(post.getViewCount())
+		// 	.likes(post.getLikes().size())
+		// 	.requestThumbnailLink(post.getStoredThumbnailPath())
+		// 	.description(post.getDescription())
+		// 	.createdAt(post.getCreatedAt())
+		// 	.countOfComments(post.getComments().size())
+		// 	.author(post.getUser().getUsername())
+		// 	.isLiked(false)
+		// 	.build();
+	}
+
+	public List<String> getEveryPages(String accessToken) throws NotionAuthenticationException {
+		NotionClient client = createClient(accessToken);
+		ArrayList<String> pageIds = new ArrayList<>();
+		List<SearchResult> results = null;
+		try (client) {
+			results = client.search("").getResults();
+		}
+
+		for (SearchResult searchResult : results) {
+			pageIds.add(searchResult.getId());
+		}
+		return pageIds;
+	}
+
+	public void initEveryPages(List<String> pageIds, Long userId, String accessCode) throws
+		NotionAuthenticationException {
+		User user = userDataRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException(USER_NOT_EXIST));
+		for (String pageId : pageIds) {
+			String title = getTitleTemp(pageId, accessCode);
+			String content = getContentTemp(pageId, accessCode);
+			Post post = postDataRepository.save(Post.builder()
+				.user(user)
+				.title(title)
+				.content(content)
+				.isPublic(true)
+				.comments(new ArrayList<>())
+				.likes(new ArrayList<>())
+				.viewCount(0L)
+				.build());
+			SyncedPages syncedPages = syncedPagesDataRepository.save(SyncedPages.builder()
+				.pageId(pageId)
+				.user(user)
+				.post(post)
+				.build());
+			post.setSyncedPages(syncedPages);
+		}
+
+	}
+
+	public boolean needUpdate(Long userId, String pageId, LocalDateTime updateTime)
+		throws NotionAuthenticationException {
+		NotionClient client = createClient(userId);
+		RetrieveBlockRequest retrieveBlockRequest = new RetrieveBlockRequest(pageId);
+		String lastEditedTime;
+		try (client) {
+			Block block = client.retrieveBlock(retrieveBlockRequest);
+			lastEditedTime = block.getLastEditedTime();
+		}
+		Instant instant = Instant.parse(lastEditedTime);
+		LocalDateTime serverUpdateTime = instant.atZone(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+		int compareResult = updateTime.compareTo(serverUpdateTime);
+
+		boolean b = compareResult < 0;
+
+		return compareResult < 0;
+		// return false;
 	}
 
 	// private 매소드
@@ -151,12 +213,53 @@ public class NotionService {
 		return stringBuilder.toString();
 	}
 
+	private String getTitleTemp(String fullPath, String accessCode) throws NotionAuthenticationException {
+		Block block;
+		NotionClient client = createClient(accessCode);
+		RetrieveBlockRequest retrieveBlockRequest = new RetrieveBlockRequest(fullPath);
+		try (client) {
+			block = client.retrieveBlock(retrieveBlockRequest);
+		}
+
+		return block.asChildPage().getChildPage().getTitle();
+	}
+
+	private String getContentTemp(String fullPath, String accessCode) throws NotionAuthenticationException {
+		Blocks blocks;
+		NotionClient client = createClient(accessCode);
+		StringBuilder stringBuilder = new StringBuilder();
+		RetrieveBlockChildrenRequest retrieveBlockChildrenRequest
+			= new RetrieveBlockChildrenRequest(fullPath);
+		try {
+			blocks = client.retrieveBlockChildren(retrieveBlockChildrenRequest);
+		} finally {
+			client.close();
+		}
+
+		List<Block> blockedContents = blocks.getResults();
+
+		for (Block blockedContent : blockedContents) {
+			stringBuilder.append(notionBlockConverter.doFilter(blockedContent, client));
+		}
+		return stringBuilder.toString();
+	}
+
 	private NotionClient createClient(Long userId) throws NotionAuthenticationException {
 		String accessToken = userDataRepository.findUserByIdForNotionAuthToken(userId)
 			.orElseThrow(NotionAuthenticationException::new)
 			.getNotionOauth()
 			.getAccessToken();
 
+		NotionClient notionClient = new NotionClient();
+		notionClient.setClientId(notionOAuthComponent.getClientId());
+		notionClient.setClientSecret(notionOAuthComponent.getClientSecret());
+		notionClient.setRedirectUri(notionOAuthComponent.getRedirectUrl());
+		notionClient.setToken(accessToken);
+
+		return notionClient;
+	}
+
+	private NotionClient createClient(String accessToken) throws NotionAuthenticationException {
 		NotionClient notionClient = new NotionClient();
 		notionClient.setClientId(notionOAuthComponent.getClientId());
 		notionClient.setClientSecret(notionOAuthComponent.getClientSecret());
