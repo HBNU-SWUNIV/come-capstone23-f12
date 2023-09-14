960519dc51b00eca7c070d1f144111c0ef8b5151
tadaHP <gusvy0220@gmail.com>
2023-09-15 01:58:08 +0900

    refactor: Exception을 공용으로 사용하지 않아 위치 초기화

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/oauth/notion/exception/TokenAvailabilityFailureException.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/oauth/notion/exception/TokenAvailabilityFailureException.java
@@ -1,4 +1,4 @@
-package io.f12.notionlinkedblog.oauth.common.exception;
+package io.f12.notionlinkedblog.oauth.notion.exception;
 
 public class TokenAvailabilityFailureException extends Exception {
 	public TokenAvailabilityFailureException() {
