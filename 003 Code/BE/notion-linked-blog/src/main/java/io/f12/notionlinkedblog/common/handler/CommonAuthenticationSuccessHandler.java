e7ccf84fbde21d104726c2eabb2b0e560536ebbf
tadaHP <gusvy0220@gmail.com>
2023-09-16 00:10:14 +0900

    feat: 디렉토리 이동 및 Ajax, OAuth2.0 공동 사용에 따른 변경

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/handler/CommonAuthenticationSuccessHandler.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/handler/CommonAuthenticationSuccessHandler.java
@@ -1,4 +1,4 @@
-package io.f12.notionlinkedblog.security.login.ajax.handler;
+package io.f12.notionlinkedblog.common.handler;
 
 import java.io.IOException;
 
@@ -15,38 +15,34 @@ import org.springframework.security.web.savedrequest.SavedRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.f12.notionlinkedblog.security.login.ajax.dto.AjaxLoginSuccessDto;
-import io.f12.notionlinkedblog.security.login.ajax.dto.LoginUser;
-import io.f12.notionlinkedblog.security.login.ajax.dto.UserWithoutPassword;
+import io.f12.notionlinkedblog.common.domain.dto.LoginSuccessDto;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AjaxAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class CommonAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private final RequestCache requestCache = new HttpSessionRequestCache();
 
-	public static AjaxAuthenticationSuccessHandler create() {
-		return new AjaxAuthenticationSuccessHandler();
+	private static CommonAuthenticationSuccessHandler self = null;
+
+	public static CommonAuthenticationSuccessHandler create() {
+		if (self == null) {
+			self = new CommonAuthenticationSuccessHandler();
+		}
+		return self;
 	}
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException {
-		log.info("AjaxAuthenticationSuccessHandler.onAuthenticationSuccess() 시작");
+		log.info("CommonAuthenticationSuccessHandler.onAuthenticationSuccess() 시작");
 		SavedRequest savedRequest = this.requestCache.getRequest(request, response);
 		String redirectUrl = savedRequest == null ? getDefaultTargetUrl() : savedRequest.getRedirectUrl();
-		AjaxLoginSuccessDto loginSuccessDto = getAjaxLoginSuccessDto(
-			authentication, redirectUrl);
+		LoginSuccessDto loginSuccessDto = LoginSuccessDto.getLoginSuccessDto(authentication, redirectUrl);
 		response.setStatus(HttpStatus.OK.value());
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		objectMapper.writeValue(response.getWriter(), loginSuccessDto);
 		clearAuthenticationAttributes(request);
-	}
-
-	private AjaxLoginSuccessDto getAjaxLoginSuccessDto(Authentication authentication, String redirectUrl) {
-		LoginUser principal = (LoginUser)authentication.getPrincipal();
-		UserWithoutPassword userWithoutPassword = UserWithoutPassword.of(principal.getUser());
-		return AjaxLoginSuccessDto.of(userWithoutPassword, redirectUrl);
 	}
 }
