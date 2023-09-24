d50a0b9f4ccd4bd2dc8f1f8065882539e2bb76d3
tadaHP <gusvy0220@gmail.com>
2023-09-25 03:44:39 +0900

    feat: 연동 정보 저장을 위한 리포지토리 생성

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/service/port/SyncedPagesRepository.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/service/port/SyncedPagesRepository.java
@@ -1,6 +1,7 @@
 package io.f12.notionlinkedblog.notion.service.port;
 
 import java.util.List;
+import java.util.Optional;
 
 import io.f12.notionlinkedblog.notion.infrastructure.SyncedPagesEntity;
 
@@ -8,4 +9,6 @@ public interface SyncedPagesRepository {
 	List<SyncedPagesEntity> findAll();
 
 	SyncedPagesEntity save(SyncedPagesEntity syncedPages);
+
+	Optional<SyncedPagesEntity> findByPageId(String pageId);
 }
