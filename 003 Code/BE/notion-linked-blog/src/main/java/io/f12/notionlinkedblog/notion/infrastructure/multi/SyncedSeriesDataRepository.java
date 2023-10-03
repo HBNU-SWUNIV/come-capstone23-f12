30bb23eb04bc8ff079bec4d1f53803ba5117f21a
tadaHP <gusvy0220@gmail.com>
2023-10-04 03:14:04 +0900

    feat: findAll 매소드 추가

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/infrastructure/multi/SyncedSeriesDataRepository.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/infrastructure/multi/SyncedSeriesDataRepository.java
@@ -1,5 +1,6 @@
 package io.f12.notionlinkedblog.notion.infrastructure.multi;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,9 @@ import io.f12.notionlinkedblog.notion.service.port.SyncedSeriesRepository;
 
 public interface SyncedSeriesDataRepository extends JpaRepository<SyncedSeriesEntity, Long>, SyncedSeriesRepository {
 
-	@Query("SELECT s FROM SyncedSeriesEntity s WHERE s.pageId =:id")
+	@Query("SELECT s from SyncedSeriesEntity s LEFT JOIN FETCH s.series LEFT JOIN FETCH s.user")
+	List<SyncedSeriesEntity> findAll();
+
+	@Query("SELECT s FROM SyncedSeriesEntity s LEFT JOIN FETCH s.user WHERE s.pageId =:id")
 	Optional<SyncedSeriesEntity> findByPageId(@Param("id") String id);
 }
