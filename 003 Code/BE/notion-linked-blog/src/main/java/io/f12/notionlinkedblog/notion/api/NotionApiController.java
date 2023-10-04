1d6f44febb4326a403e03548c63e0e8000cd29a8
tadaHP <gusvy0220@gmail.com>
2023-10-04 23:42:31 +0900

    fix: 필요없는 매서드 삭제

--- 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/api/NotionApiController.java
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/notion/api/NotionApiController.java
@@ -1,10 +1,8 @@
 package io.f12.notionlinkedblog.notion.api;
 
-import static io.f12.notionlinkedblog.common.exceptions.message.ExceptionMessages.UserValidateMessages.*;
 import static org.springframework.util.MimeTypeUtils.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
@@ -96,12 +94,6 @@ public class NotionApiController {
 
 	private void notionAccessAvailable(LoginUser loginUser) {
 		Assert.notNull(loginUser.getUser().getNotionOauth().getAccessToken(), "AuthenticationNeed");
-	}
-
-	private void checkSameUser(Long id, LoginUser loginUser) {
-		if (!id.equals(loginUser.getUser().getId())) {
-			throw new AccessDeniedException(USER_NOT_MATCH);
-		}
 	}
 
 }
