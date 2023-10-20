ab01146b47a50340bc87a9590e7584145101d408
tadaHP <gusvy0220@gmail.com>
2023-10-20 22:47:38 +0900

    feat: aws s3 관련 기능, 설정 작성 1. AwsBucket: 이미지 파일이름을 주소로 변경하는 기능 2. AwsConfig: AwsS3관련 설정파일

---
+++ 003 Code/BE/notion-linked-blog/src/main/java/io/f12/notionlinkedblog/common/domain/AwsBucket.java
@@ -0,0 +1,17 @@
+package io.f12.notionlinkedblog.common.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AwsBucket {
+	@Value("${application.bucket.name}")
+	private String bucket;
+	@Value("${cloud.aws.region.static}")
+	private String region;
+
+	public String makeFileUrl(String fileFullName) {
+		return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + fileFullName;
+	}
+
+}
